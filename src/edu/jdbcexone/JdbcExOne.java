de302d03543dd9aeba55bca4e579978cdc80b7d6
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-22 17:41:40 +0900

    Jdbc HW

--- src/edu/jdbcexone/JdbcExOne.java
+++ src/edu/jdbcexone/JdbcExOne.java
@@ -4,18 +4,28 @@ import java.sql.*;
 
 public class JdbcExOne {
     private static void printData(
-            ResultSet srs, String col1, String col2, String col3) throws SQLException {
-        System.out.println("name\t|\t\tid\t\t|\t   dept");
-        System.out.println("=======================================");
+            ResultSet srs, String col1, String col2, String col3, String col4, String col5, String col6, String col7 ) throws SQLException {
         while (srs.next()) {
             if (!col1.equals(""))
-                System.out.print(srs.getString("name"));
+                System.out.print(srs.getString("userID"));
             if (!col2.equals(""))
                 System.out.print("\t|\t" +
-                        srs.getString("id"));
+                        srs.getString("name"));
             if (!col3.equals(""))
+                System.out.print("\t|\t" +
+                        srs.getString("birthYear"));
+            if (!col4.equals(""))
+                System.out.print("\t|\t" +
+                        srs.getString("address"));
+            if (!col5.equals(""))
+                System.out.print("\t|\t" +
+                        srs.getString("mobile1"));
+            if (!col6.equals(""))
+                System.out.print("-"+
+                        srs.getString("mobile2"));
+            if (!col7.equals(""))
                 System.out.println("\t|\t" +
-                        srs.getString("dept"));
+                        srs.getString("mDate"));
             else
                 System.out.println();
         }//end of while(rs.next() )
@@ -31,7 +41,7 @@ public class JdbcExOne {
             //forName은 jdbc드라이버에 접속할 수 있도록 해줌
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            String url = "jdbc:mysql://localhost:3306/sampledb";
+            String url = "jdbc:mysql://localhost:3306/malldb";
             String userName = "root";
             String password = "gging00100!";
 
@@ -40,8 +50,8 @@ public class JdbcExOne {
             if (conn != null){
                 System.out.println("DB 연결 완료");
                 stmt = conn.createStatement();
-                ResultSet srs = stmt.executeQuery("select * from student");
-                printData(srs, "name", "id", "dept");
+                ResultSet srs = stmt.executeQuery("select * from usertbl");
+                printData(srs, "userID", "name", "birthYear","address","mobile1","mobile2", "mDate" );
             }
 
         }catch (ClassNotFoundException e){
