3541513ea069c04622c076d73da433f6c751fad2
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-02-22 15:30:47 +0900

    connact

--- src/edu/jdbcexone/JdbcExOne.java
+++ src/edu/jdbcexone/JdbcExOne.java
@@ -1,7 +1,39 @@
 package edu.jdbcexone;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcExOne {
     public static void main(String[] args){
         System.out.println("JDBC Start");
+
+        Connection conn = null;
+
+        try{
+            //forName은 jdbc드라이버에 접속할 수 있도록 해줌
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost:3306/sampledb";
+            String userName = "root";
+            String password = "gging00100!";
+
+            conn = DriverManager.getConnection(url, userName, password);
+
+            if (conn != null){
+                System.out.println("DB 연결 완료");
+            }
+
+        }catch (ClassNotFoundException e){
+            System.out.println("JDBC 드라이버 로드 오류");
+        }catch (SQLException e){
+            System.out.println("DB 연결 오류");
+        }finally{
+            try{
+                conn.close();
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
